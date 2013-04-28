cf9527fe0c810a69c91fdb57e27b513aeb88a4c4
Cam Spiers <camspiers@gmail.com>
2013-04-28 13:06:20 +1200

    UPDATE: Change names

--- _config.php
+++ _config.php
@@ -1,7 +1,1 @@
 <?php
-
-Director::addRules(70, array(
-	SilverStripeWkHtmlToPdfController::$url_segment . '//$Action/$ID/$OtherID' => 'SilverStripeWkHtmlToPdfController'
-));
-
-Object::add_extension('Image', 'SilverStripeWkHtmlToPdfTemplateHelper_ImageExtension');
