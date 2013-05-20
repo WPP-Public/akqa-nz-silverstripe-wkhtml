716df6088551d991189267121f9d2107a7058899
Stephen <stephen@heyday.co.nz>
2013-05-20 12:13:08 +1200

    UPDATE: Mime type guessing for data URL helper

--- src/Heyday/SilverStripe/WkHtml/TemplateHelper.php
+++ src/Heyday/SilverStripe/WkHtml/TemplateHelper.php
@@ -21,13 +21,46 @@ class TemplateHelper extends ViewableData
         }
     }
     /**
-     * @param $image
+     * Make a data URL from an image file
+     * @param $path
      * @return string
      */
-    public function EmbedBase64Image($image)
+    public function EmbedBase64Image($path)
     {
-        if (file_exists(BASE_PATH . $image)) {
-            return 'data:image/png;base64,' . base64_encode(file_get_contents(BASE_PATH . $image));
+        $path = BASE_PATH . $path;
+
+        if (file_exists($path)) {
+            $mime = $this->getMimeType($path);
+
+            if ($mime) {
+                return 'data:'.$mime.';base64,' . base64_encode(file_get_contents($path));
+            }
+        }
+    }
+    /**
+     * Guess mime type based on file extension
+     * @param  $path
+     * @return string
+     */
+    public function getMimeType($path)
+    {
+        $mimeTypes = array(
+            'png' => 'image/png',
+            'jpe' => 'image/jpeg',
+            'jpeg' => 'image/jpeg',
+            'jpg' => 'image/jpeg',
+            'gif' => 'image/gif',
+            'bmp' => 'image/bmp',
+            'ico' => 'image/vnd.microsoft.icon',
+            'tiff' => 'image/tiff',
+            'tif' => 'image/tiff',
+            'svg' => 'image/svg+xml',
+            'svgz' => 'image/svg+xml'
+        );
+
+        $ext = pathinfo($path, PATHINFO_EXTENSION);
+        if (array_key_exists($ext, $mimeTypes)) {
+            return $mimeTypes[$ext];
         }
     }
 }
